f32581a4ce680ce44d397e7d8f393c7e799ad3d1
Josh Glazer <jsg@JSG-Desktop.local>
2019-05-13 21:04:51 -0400

    add sub1

--- PforDA/PfDA_Ch02.py
+++ PforDA/PfDA_Ch02.py
@@ -1,12 +1,16 @@
-% yo
+import numpy as np
+
+
 an_apple = 27
 an_example = 42
 an_orange = an_apple * 2
 # print(an_orange)
 
 b = [1,2,3]
-
+#b?
 # print(b.count)
 # print(an_apple?)
-print("type ")
-print(type(b))
+#print("type ")
+#print(type(b))
+
+%cat Sub01.py
---
+++ PforDA/Sub01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 20:51:00 2019
+
+@author: jsg
+"""
+
+def f(x, y, z):
+    return (x + y) / z
+
+a = 5
+b = 6
+c = 7.5
+
+result = f(a, b, c)
+print(result)
